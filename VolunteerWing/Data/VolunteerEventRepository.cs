4b8a69d8cdc375df03a8c4a30faeae7190564118
Ripal <ripal212007@gmail.com>
2019-07-15 20:56:37 -0500

    added volunteer event repository with add event method

--- VolunteerWing/Data/VolunteerEventRepository.cs
+++ VolunteerWing/Data/VolunteerEventRepository.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using VolunteerWing.Models;
 
 namespace VolunteerWing.Data
 {
     public class VolunteerEventRepository
     {
+        readonly string _connectionString;
+
+        public VolunteerEventRepository(IOptions<DbConfiguration> dbConfig)
+        {
+            _connectionString = dbConfig.Value.ConnectionString;
+        }
+
+        public VolunteerEvent AddVolunteerEvent(string eventName, DateTime startDate, TimeSpan startTime, TimeSpan endTime, int adminId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var newVolunteerEvent = db.QueryFirstOrDefault<VolunteerEvent>(@"
+                    insert into volunteerEvent (eventName, startDate, startTime, endTime, adminId)
+                    output inserted.*
+                    values (@eventName, @startDate, @startTime, @endTime, @adminId)",
+                    new { eventName, startDate, startTime, endTime, adminId });
+
+                if (newVolunteerEvent != null)
+                {
+                    return newVolunteerEvent;
+                }
+            }
+            throw new Exception("No volunteer event created");
+        }
     }
 }
