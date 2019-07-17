bff44421ce7db3e5c6bb74ab2eea318dfbd6a80f
Ripal <ripal212007@gmail.com>
2019-07-17 14:27:38 -0500

    added eslint, scss and update npm package

---
+++ VolunteerWing/ClientApp/src/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Home } from '../components/Home';
+
+
+export default class App extends Component {
+  displayName = App.name
+
+  render() {
+    return (
+      <div>
+        <Route exact path='/' component={Home} />
+      </div>
+    );
+  }
+}
