0ec1ee366855e4a95411267d90085987a0cc7976
Ripal <ripal212007@gmail.com>
2019-07-19 21:33:42 -0500

    styled card by doing flex and center

--- VolunteerWing/ClientApp/src/components/pages/Home/Home.js
+++ VolunteerWing/ClientApp/src/components/pages/Home/Home.js
@@ -10,22 +10,22 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div className="home mx-auto">
-        <div className="card-columns homeWrapper mt-5">
-          <div className="card mt-3 border-dark animated zoomIn" id="profile" onClick={this.changeView}>
-            <div className="card-body home text-center">
-              <h4 className="card-title"><i className="fas fa-user fa-6x"></i></h4>
-              <h5 className="card-subtitle mb-2 text-muted">Profile</h5>
-              <p className="card-text">View Profile</p>
+      <div className="home w-75 mx-auto">
+        <div className="homeWrapper mt-5 d-flex flex-wrap justify-content-center">
+            <div className="card mt-3 border-dark animated zoomIn" id="profile" onClick={this.changeView}>
+              <div className="card-body home text-center">
+                <h4 className="card-title"><i className="fas fa-user fa-6x"></i></h4>
+                <h5 className="card-subtitle mb-2 text-muted">Profile</h5>
+                <p className="card-text">View Profile</p>
+              </div>
             </div>
-          </div>
-          <div className="card mt-3 border-dark animated zoomIn" id="createEvent" onClick={this.changeView}>
-            <div className="card-body home text-center">
-              <h4 className="card-title"><i className="fas fa-plus-circle fa-6x"></i></h4>
-              <h5 className="card-subtitle mb-2 text-muted">Create Event</h5>
-              <p className="card-text">Add your event here to volunteer</p>
+            <div className="card mt-3 border-dark animated zoomIn" id="createEvent" onClick={this.changeView}>
+              <div className="card-body home text-center">
+                <h4 className="card-title"><i className="fas fa-plus-circle fa-6x"></i></h4>
+                <h5 className="card-subtitle mb-2 text-muted">Create Event</h5>
+                <p className="card-text">Add your event here to volunteer</p>
+              </div>
             </div>
-          </div>
         </div>
       </div>
     );
