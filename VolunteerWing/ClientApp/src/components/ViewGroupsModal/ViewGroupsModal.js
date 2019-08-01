79d992bd40ff29ba3cd433c7a092b1209f553b3d
Ripal <ripal212007@gmail.com>
2019-08-01 11:19:37 -0500

    on home view group icon is displaying and clicking on it toggles group modal

---
+++ VolunteerWing/ClientApp/src/components/ViewGroupsModal/ViewGroupsModal.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+} from 'reactstrap';
+import groupRequests from '../../helpers/data/userTaskRequests';
+import SingleGroup from '../SingleGroup/SingleGroup';
+import './ViewGroupsModal.scss';
+
+class SignupModal extends React.Component {
+  viewGroupModalMounted = false;
+
+  static propTypes = {
+    toggleViewGroupModal: PropTypes.func,
+    viewGroupModal: PropTypes.bool,
+    currentUser: PropTypes.object,
+  }
+
+  state = {
+    groups: [],
+  }
+
+  // getGroupsByAdminId = () => {
+  //   const { currentUser } = this.props;
+  //   const adminId = currentUser.id;
+  //   groupRequests.getAllGroupsByAdminId(adminId)
+  //     .then((groups) => {
+  //       this.setState({ groups });
+  //     });
+  // }
+
+  toggleEvent = () => {
+    const { toggleViewGroupModal } = this.props;
+    toggleViewGroupModal();
+  }
+
+  componentDidMount() {
+    const { currentUser } = this.props;
+    this.viewGroupModalMounted = !!currentUser.id;
+    if (this.viewGroupModalMounted) {
+      // this.getGroupsByAdminId();
+    }
+  }
+
+  render() {
+    const { viewGroupModal } = this.props;
+    const { groups } = this.state;
+
+    return (
+        <Modal isOpen={viewGroupModal} toggle={this.toggleEvent} className="modal-lg">
+        <ModalHeader className="modal-header text-center" toggle={this.toggleEvent}> My Groups</ModalHeader>
+        <ModalBody className="modal-body">
+          <div>
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                <th scope="col">Group Name</th>
+                <th scope="col">Members</th>
+                <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* {singleEventComponent} */}
+              </tbody>
+            </table>
+          </div>
+        </ModalBody>
+    </Modal>
+    );
+  }
+}
+
+export default SignupModal;
